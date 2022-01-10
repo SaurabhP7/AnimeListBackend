ebd12f199721a6c647cc2c00dab4e2e5e59b534a
YourGoodFriendSP <71482137+YourGoodFriendSP@users.noreply.github.com>
2022-01-10 21:14:05 +0530

    added optimized build for Heroku

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
-
 
 app.use(middleware.tokenExtractor)
 app.use('/api/login',loginRouter)
